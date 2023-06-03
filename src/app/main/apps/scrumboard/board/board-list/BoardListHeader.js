de8899c415aee6320eeb78e7f998ab2667a55b5e
nhatnh <nhatnh@mismart.ai>
2023-06-03 20:11:43 +0700

    update

--- src/app/main/apps/scrumboard/board/board-list/BoardListHeader.js
+++ src/app/main/apps/scrumboard/board/board-list/BoardListHeader.js
@@ -15,9 +15,58 @@ import { useDispatch } from 'react-redux';
 import * as yup from 'yup';
 import _ from '@lodash';
 import { Box } from '@mui/system';
-import { darken } from '@mui/material/styles';
+import { darken, styled } from '@mui/material/styles';
 import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
+import { FormControlLabel, Switch } from '@mui/material';
 import { removeList, updateList } from '../../store/listsSlice';
+
+const IOSSwitch = styled((props) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 42,
+  height: 26,
+  padding: 0,
+  '& .MuiSwitch-switchBase': {
+    padding: 0,
+    margin: 2,
+    transitionDuration: '300ms',
+    '&.Mui-checked': {
+      transform: 'translateX(16px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+        opacity: 1,
+        border: 0,
+      },
+      '&.Mui-disabled + .MuiSwitch-track': {
+        opacity: 0.5,
+      },
+    },
+    '&.Mui-focusVisible .MuiSwitch-thumb': {
+      color: '#33cf4d',
+      border: '6px solid #fff',
+    },
+    '&.Mui-disabled .MuiSwitch-thumb': {
+      color: theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[600],
+    },
+    '&.Mui-disabled + .MuiSwitch-track': {
+      opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxSizing: 'border-box',
+    width: 22,
+    height: 22,
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 26 / 2,
+    backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+    opacity: 1,
+    transition: theme.transitions.create(['background-color'], {
+      duration: 500,
+    }),
+  },
+}));
 
 /**
  * Form Validation Schema
@@ -78,6 +127,10 @@ function BoardListHeader(props) {
     const newDataTwo = { ...newData, boardId: list.boardId, id: list.id };
     dispatch(updateList({ id: list.id, newDataTwo }));
     handleCloseForm();
+  }
+
+  function handleSettingForm(listData) {
+    console.log('handleSettingForm', listData);
   }
 
   return (
@@ -165,6 +218,12 @@ function BoardListHeader(props) {
                 <FuseSvgIcon>heroicons-outline:pencil</FuseSvgIcon>
               </ListItemIcon>
               <ListItemText primary="Rename List" />
+            </MenuItem>
+            <MenuItem onClick={handleSettingForm(list)}>
+              <FormControlLabel
+                control={<IOSSwitch sx={{ m: 1 }} defaultChecked />}
+                label="Draggable"
+              />
             </MenuItem>
           </Menu>
         </div>
--- src/app/main/apps/service-ai/ServiceAIApp.js
+++ src/app/main/apps/service-ai/ServiceAIApp.js
@@ -6,7 +6,7 @@ function ServiceAIApp() {
   return (
     <Box sx={{ width: '100%', backgroundColor: '#FFF', overflow: 'hidden' }}>
       <iframe
-        src="https://d31e6b24a00ea19456.gradio.live"
+        src="https://npssocial.site/"
         title="Full Screen ServiceAi"
         style={{ width: '100%', height: '100vh', border: 'none' }}
       />
